08564019fa8df23bfd359c06eecfc65033cef2cb
Mohit-e-Gupta <mohit.e.gupta@gmail.com>
2016-08-11 16:59:19 +0530

    done

--- AngularAlite/AngularAlite/app/App.js
+++ AngularAlite/AngularAlite/app/App.js
@@ -2,35 +2,17 @@ var TaskApp = angular.module('TaskApp', ['ngRoute', 'datatables', 'ngAnimate', '
 
 TaskApp.config(['$routeProvider', function ($routeProvider) {
 
-    $routeProvider.when("/table/basic", {
-        templateUrl: 'views/dashboard/basictable.html',
-        controller: 'tableCtrl'
-    }).when("/table/dynamic", {
+    $routeProvider.when("/table/dynamic", {
         templateUrl: 'app/pages/tables/table.html'
     }).when("/ui/buttons", {
-        templateUrl: 'views/ui-elements/button.html'
+        templateUrl: 'app/pages/ui/button.html'
     }).when("/ui/notifications", {
-        templateUrl: 'views/ui-elements/notifications.html'
-    }).when('/service-code-new', {
-        templateUrl: 'views/dashboard/blockForm.html'
+        templateUrl: 'app/pages/ui/notifications.html'
     }).when('/forms/advanced', {
-        templateUrl: 'views/dashboard/advanced.html'
+        templateUrl: 'app/pages/forms/advanced.html'
     }).otherwise({ redirectTo: '/table/dynamic' });
 
 }]);
 
-TaskApp.config(['toastrConfig', function (toastrConfig) {
-    angular.extend(toastrConfig, {
-        autoDismiss: false,
-        closeButton: true,
-        progressBar: true,
-        containerId: 'toast-container',
-        maxOpened: 0,
-        newestOnTop: true,
-        positionClass: 'toast-top-full-width',
-        preventDuplicates: false,
-        preventOpenDuplicates: false,
-        target: 'body'
-    });
-}]);
+
 
